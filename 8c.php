61b2a2b713a4199b7e8e3805a6a7906964d04a88
kishoren26 <39940759+kishoren26@users.noreply.github.com>
2018-11-16 12:46:58 +0530

    Add files via upload

---
+++ 8c.php
@@ -0,0 +1,41 @@
+<?php
+$mat1=array(array(1,2,8),
+	     array(4,5,6),
+             array(7,8,3));
+$mat2=array(array(1,1,1),
+	    array(1,1,1),
+	    array(1,1,1));
+
+echo"<h2>Matrix1</h2>";
+for($row=0;$row<3;$row++)
+	{
+		echo "<br>";
+		for($col=0;$col<3;$col++)
+		{
+			echo"".$mat1[$row][$col];
+		}
+	}
+echo"<h2>Matrix2</h2>";
+for($row=0;$row<3;$row++)
+	{
+		echo "<br>";
+		for($col=0;$col<3;$col++)
+		{
+			echo"".$mat2[$row][$col];
+		}
+	}
+$result = array();
+for($row=0; $row<=2; $row++) {
+    for($col=0; $col<=2; $col++) {
+        $result[$row][$col] = $mat1[$row][$col] + $mat2[$row][$col];
+    }
+}
+echo"<h2>result</h2>";
+for($row=0;$row<3;$row++)
+	{
+		echo "<br>";
+		for($col=0;$col<3;$col++)
+		{
+			echo"".$result[$row][$col];
+		}
+	}
